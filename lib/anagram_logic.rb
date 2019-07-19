21a872a292151a04e49a63fc525cf0009a1a7134
FatherofDharma <fatherofdharma@gmail.com>
2019-07-19 09:56:25 -0700

    remove the initialization argument for class, and add two arguments to the anagram method. This allows test_argument to be called internally more easily.

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -1,17 +1,24 @@
 require 'pry'
 
 class Word_test
-  def initialize(first_word)
-    @first_word = first_word
+  def initialize
   end
 
-  def test_word
-    if /[aeiouy]/.match(@first_word)
+  def test_word(word)
+    if /[aeiouy]/.match(word)
       true
+    else
+      false
     end
   end
 
-  def anagram(second_word)
+  def anagram(first_word, second_word)
+    @first_word = first_word
+    @second_word = second_word
+
+    if test_word(@first_word)
+      firstword = @first_word.split('')
+    end
   end
 
 
--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -5,8 +5,13 @@ require 'pry'
 describe('Word_test') do
 
   it ('should accurately identify if a word is valid, by identifying if it contains at least one vowel') do
-    example = Word_test.new('dog')
-  expect(example.test_word()).to(eq(true))
+    example = Word_test.new()
+  expect(example.test_word('dog')).to(eq(true))
+  end
+
+  it ('should accurately identify if a word is invalid, by identifying if it does not contain at least one vowel') do
+    example = Word_test.new()
+  expect(example.test_word('dgt')).to(eq(false))
   end
 
   # it ('should accurately identify if two words contain the same letters') do
