7498cb284482a6c90ef7c6f651968ca23ad22ff1
FatherofDharma <fatherofdharma@gmail.com>
2019-07-19 09:12:08 -0700

    add code for first test. Sidenote: whiteboarded some initial solution ideas.

--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -1,3 +1,12 @@
 require 'rspec'
 require 'anagram_logic'
 require 'pry'
+
+describe('Word_test') do
+
+  it ('should accurately identify if two words contain the same letters') do
+    test = Word_test.new('dog')
+  expect(test.anagram('god')).to(eq(true))
+  end
+
+end
