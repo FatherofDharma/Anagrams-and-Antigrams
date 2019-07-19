55c3a29ab476ca47226f9721e452f10238c4d33a
FatherofDharma <fatherofdharma@gmail.com>
2019-07-19 09:27:12 -0700

    add method for testing words. Correctly failed spec then passed.

--- lib/anagram_logic.rb
+++ lib/anagram_logic.rb
@@ -1,1 +1,18 @@
 require 'pry'
+
+class Word_test
+  def initialize(first_word)
+    @first_word = first_word
+  end
+
+  def test_word
+    if /[aeiouy]/.match(@first_word)
+      true
+    end
+  end
+
+  def anagram(second_word)
+  end
+
+
+end
--- spec/anagram_logic_spec.rb
+++ spec/anagram_logic_spec.rb
@@ -5,13 +5,13 @@ require 'pry'
 describe('Word_test') do
 
   it ('should accurately identify if a word is valid, by identifying if it contains at least one vowel') do
-    test = Word_test.new('dog')
-  expect(test.anagram('god')).to(eq(true))
+    example = Word_test.new('dog')
+  expect(example.test_word()).to(eq(true))
   end
 
-  it ('should accurately identify if two words contain the same letters') do
-    test = Word_test.new('dog')
-  expect(test.anagram('god')).to(eq(true))
-  end
+  # it ('should accurately identify if two words contain the same letters') do
+  #   test = Word_test.new('dog')
+  # expect(test.anagram('god')).to(eq(true))
+  # end
 
 end
